5a121de8d8b2b8af3d49d46219cf9166bbe9e076
komination <kominetti@proton.me>
2025-05-05 05:59:01 +0000

    feat(terraform): VPCエンドポイントと関連リソースの追加

--- terraform/env/dev/main.tf
+++ terraform/env/dev/main.tf
@@ -4,5 +4,24 @@ module "vpc" {
   vpc_cidr = "10.0.0.0/16"
   enable_nat_gateway = false
   one_nat_gateway_per_az = false
-  
+}
+
+// S3用 VPCエンドポイント
+module "vpc_endpoint_s3" {
+  source          = "../../modules/vpc_endpoint_s3"
+  vpc_id          = module.vpc.vpc_id
+  route_table_ids = module.vpc.private_subnets
+}
+
+// デフォルトセキュリティグループを取得
+resource "aws_default_security_group" "default" {
+  vpc_id = module.vpc.vpc_id
+}
+
+// Lambda用 VPCエンドポイント
+module "vpc_endpoint_lambda" {
+  source                   = "../../modules/vpc_endpoint_lambda"
+  vpc_id                   = module.vpc.vpc_id
+  lambda_subnet_ids        = module.vpc.private_subnets
+  lambda_security_group_ids = [aws_default_security_group.default.id]
 }
---
+++ terraform/modules/vpc_endpoint_s3/main.tf
@@ -0,0 +1,11 @@
+// デフォルトリージョンを取得
+
+data "aws_region" "current" {
+}
+
+resource "aws_vpc_endpoint" "s3" {
+  vpc_id            = var.vpc_id
+  service_name      = "com.amazonaws.${data.aws_region.current.name}.s3"
+  vpc_endpoint_type = "Gateway"
+  route_table_ids   = var.route_table_ids
+}

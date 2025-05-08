652fd14b638e88c277f73a86a8aaaeb854758c62
komination <kominetti@proton.me>
2025-05-08 07:32:57 +0000

    fix(terraform): プライベートルートテーブルIDの変数名を修正し、ポリシーを追加

--- terraform/modules/vpc_endpoint_s3/main.tf
+++ terraform/modules/vpc_endpoint_s3/main.tf
@@ -7,5 +7,20 @@ resource "aws_vpc_endpoint" "s3" {
   vpc_id            = var.vpc_id
   service_name      = "com.amazonaws.${data.aws_region.current.name}.s3"
   vpc_endpoint_type = "Gateway"
-  route_table_ids   = var.route_table_ids
+  route_table_ids   = var.private_route_table_ids
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = [
+          "s3:GetObject",
+          "s3:PutObject",
+          "s3:ListBucket"
+        ]
+        Resource  = var.allowed_bucket_arns
+      }
+    ]
+  })
 }

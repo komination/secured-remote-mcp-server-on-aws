79b158d3f295f589ae7913f372afd385dd6c0bed
komination <kominetti@proton.me>
2025-05-08 07:53:52 +0000

    feat(terraform): Lambda, Lambda Layer, S3の実装。

--- terraform/env/dev/main.tf
+++ terraform/env/dev/main.tf
@@ -2,26 +2,104 @@ module "vpc" {
   source = "../../modules/vpc"
   stage = "dev"
   vpc_cidr = "10.0.0.0/16"
-  enable_nat_gateway = false
-  one_nat_gateway_per_az = false
 }
 
-// S3用 VPCエンドポイント
 module "vpc_endpoint_s3" {
   source          = "../../modules/vpc_endpoint_s3"
   vpc_id          = module.vpc.vpc_id
-  route_table_ids = module.vpc.private_subnets
+  private_route_table_ids = module.vpc.private_route_table_ids
+  allowed_bucket_arns = [module.s3.bucket_arn, "${module.s3.bucket_arn}/*"]
 }
 
-// デフォルトセキュリティグループを取得
-resource "aws_default_security_group" "default" {
-  vpc_id = module.vpc.vpc_id
-}
-
-// Lambda用 VPCエンドポイント
 module "vpc_endpoint_lambda" {
   source                   = "../../modules/vpc_endpoint_lambda"
   vpc_id                   = module.vpc.vpc_id
   lambda_subnet_ids        = module.vpc.private_subnets
-  lambda_security_group_ids = [aws_default_security_group.default.id]
+  lambda_security_group_ids = [module.vpc.aws_default_security_group_id]
+}
+
+module "s3" {
+  source = "../../modules/s3"
+  bucket_name        = "aws-vpc-lambda-integration-dev"
+  enable_versioning  = false
+}
+
+data "archive_file" "lambda_layer_zip" {
+  type        = "zip"
+  source_dir  = "${path.module}/../../../src/layer"
+  output_path = "${path.module}/.terraform-artifacts/layer.zip"
+  excludes = [
+    "*.pyc",
+    "__pycache__/**",
+    "*.toml",
+    "*.lock",
+    ".venv/**",
+  ]
+}
+
+data "archive_file" "lambda_zip" {
+  type        = "zip"
+  source_dir  = "${path.module}/../../../src"
+  output_path = "${path.module}/.terraform-artifacts/lambda.zip"
+  excludes = [
+    "*.pyc",
+    "__pycache__/**",
+    "*.toml",
+    "*.lock",
+    "layer/**",
+    ".venv/**",
+    "requirements.txt",
+  ]
+}
+
+resource "aws_s3_object" "lambda_layer_archive" {
+  bucket = module.s3.bucket_id
+  key    = "lambda-layers/dev-my-lambda-layer.zip"
+  source = data.archive_file.lambda_layer_zip.output_path
+
+  depends_on = [ 
+    module.s3 
+  ]
+}
+
+resource "aws_s3_object" "lambda_function_archive" {
+  bucket = module.s3.bucket_id
+  key    = "lambda-functions/dev-my-lambda-fn.zip"
+  source = data.archive_file.lambda_zip.output_path
+
+  depends_on = [ 
+    module.s3 
+  ]
+}
+
+module "lambda_layer" {
+  source             = "../../modules/lambda_layer"
+  layer_name         = "dev-my-lambda-layer"
+  runtime            = "python3.13"
+  layer_source_path  = data.archive_file.lambda_layer_zip.output_path
+  s3_bucket_name     = module.s3.bucket_id
+  depends_on = [ 
+    aws_s3_object.lambda_layer_archive 
+  ]
+}
+
+module "lambda" {
+  source                    = "../../modules/lambda"
+  function_name             = "dev-my-lambda-fn"
+  handler                   = "lambda_function.lambda_handler"
+  runtime                   = "python3.13"
+  s3_bucket_name            = module.s3.bucket_id
+  s3_key                    = aws_s3_object.lambda_function_archive.key
+  layers                    = [module.lambda_layer.layer_arn]
+  vpc_subnet_ids            = module.vpc.private_subnets
+  vpc_security_group_id     = module.vpc.aws_default_security_group_id
+  environment_variables     = {
+    "BUCKET_NAME" = module.s3.bucket_id
+  }
+  depends_on = [ 
+    module.vpc, 
+    module.lambda_layer, 
+    module.vpc_endpoint_lambda, 
+    aws_s3_object.lambda_function_archive 
+  ]
 }

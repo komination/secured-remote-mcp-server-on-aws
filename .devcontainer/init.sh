0a7f031aa7e2c5f3c0d842d0420e7d0ebbb66a01
komination <kominetti@proton.me>
2025-05-15 06:41:42 +0000

    fix(devcontainer): fix env name

--- .devcontainer/init.sh
+++ .devcontainer/init.sh
@@ -7,13 +7,13 @@ cat <<EOL > ~/.aws/config
 region=${AWS_DEFAULT_REGION}
 output=${AWS_DEFAULT_OUTPUT}
 
-[profile ${ADMIN_PROFILE_NAME}]
-sso_session = ${ADMIN_PROFILE_NAME}
+[profile ${SSO_PROFILE_NAME}]
+sso_session = ${SSO_PROFILE_NAME}
 sso_account_id = ${SSO_ACCOUNT_ID}
 sso_role_name = ${SSO_ROLE_NAME}
 region = ${AWS_DEFAULT_REGION}
 
-[sso-session ${ADMIN_PROFILE_NAME}]
+[sso-session ${SSO_PROFILE_NAME}]
 sso_start_url = ${SSO_START_URL}
 sso_region = ${AWS_DEFAULT_REGION}
 sso_registration_scopes = sso:account:access

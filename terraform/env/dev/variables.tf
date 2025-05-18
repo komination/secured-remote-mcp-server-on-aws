73a2e58504fe3817cd42a02d8776f24e7c711252
komination <kominetti@proton.me>
2025-05-18 07:16:43 +0000

    feat(workflow): add inputs for Lambda ZIP and Layer keys in reusable workflow

--- terraform/env/dev/variables.tf
+++ terraform/env/dev/variables.tf
@@ -1,11 +1,13 @@
 variable "lambda_zip_key" {
   description = "S3 object key for the Lambda function ZIP (Run-specific)"
   type        = string
-  default     = "lambda/dev-my-lambda-fn-dummy.zip" # 初期デプロイ用ダミー
+  default     = "lambda/dev-my-lambda-fn-dummy.zip"
+  nullable    = false
 }
 
 variable "lambda_layer_zip_key" {
   description = "S3 object key for the Lambda layer ZIP (Run-specific)"
   type        = string
   default     = "lambda-layers/dev-my-lambda-layer-dummy.zip"
+  nullable    = false
 }

3f3b6fb0e97c66e39535b8d857ac2a3abb2caea0
komination <kominetti@proton.me>
2025-05-19 08:18:12 +0000

    feat(workflow):  reusable workflows

---
+++ terraform/env/dev/outputs.tf
@@ -0,0 +1,8 @@
+output "lambda_function_name" {
+  value = module.lambda.function_name
+
+}
+
+output "lambda_layer_name" {
+  value = module.lambda_layer.layer_name
+}
--- terraform/modules/lambda_layer/outputs.tf
+++ terraform/modules/lambda_layer/outputs.tf
@@ -4,3 +4,8 @@ output "layer_arn" {
   description = "作成したLambda LayerのARN"
   value       = module.lambda_layer_s3.lambda_layer_arn
 }
+
+output "layer_name" {
+  description = "作成したLambda Layerの名前"
+  value       = module.lambda_layer_s3.s3_object
+}
